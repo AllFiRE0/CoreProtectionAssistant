2f65b1f0a5ee23268cd5bb45f30975f7d29bc778
AllF1RE <111210671+AllFiRE0@users.noreply.github.com>
2026-04-07 23:40:17 +0300

    Create ReportCommand.java

---
+++ src/main/java/ru/allfire/coreprotectionassistant/commands/ReportCommand.java
@@ -0,0 +1,93 @@
+package ru.allfire.coreprotectionassistant.commands;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+import ru.allfire.coreprotectionassistant.CoreProtectionAssistant;
+import ru.allfire.coreprotectionassistant.utils.Color;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ReportCommand implements CommandExecutor, TabCompleter {
+    
+    private final CoreProtectionAssistant plugin;
+    
+    public ReportCommand(CoreProtectionAssistant plugin) {
+        this.plugin = plugin;
+    }
+    
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command,
+                             @NotNull String label, @NotNull String[] args) {
+        
+        if (!(sender instanceof Player player)) {
+            sender.sendMessage(Color.colorize("&cThis command can only be used by players"));
+            return true;
+        }
+        
+        if (!player.hasPermission("cpa.report")) {
+            player.sendMessage(Color.colorize(
+                plugin.getConfigManager().getLangConfig().getString("messages.no_permission",
+                    "%prefix% &cNo permission")
+            ));
+            return true;
+        }
+        
+        if (args.length < 2) {
+            player.sendMessage(Color.colorize(
+                plugin.getConfigManager().getLangConfig().getString("messages.report_usage",
+                    "%prefix% &cUsage: /report <player> <reason>")
+            ));
+            return true;
+        }
+        
+        String targetName = args[0];
+        Player target = Bukkit.getPlayer(targetName);
+        
+        if (target == null || !target.isOnline()) {
+            player.sendMessage(Color.colorize(
+                plugin.getConfigManager().getLangConfig().getString("messages.player_offline",
+                    "%prefix% &cPlayer is offline")
+                    .replace("%player%", targetName)
+            ));
+            return true;
+        }
+        
+        if (target.equals(player)) {
+            player.sendMessage(Color.colorize("&cYou cannot report yourself"));
+            return true;
+        }
+        
+        String reason = String.join(" ", Arrays.copyOfRange(args, 1, args.length));
+        
+        var result = plugin.getReportManager().createReport(player, target, reason);
+        
+        player.sendMessage(Color.colorize(result.message()));
+        
+        return true;
+    }
+    
+    @Override
+    public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command,
+                                      @NotNull String label, @NotNull String[] args) {
+        
+        if (args.length == 1) {
+            return Bukkit.getOnlinePlayers().stream()
+                .map(Player::getName)
+                .filter(n -> n.toLowerCase().startsWith(args[0].toLowerCase()))
+                .filter(n -> !n.equals(sender.getName()))
+                .toList();
+        }
+        
+        if (args.length == 2) {
+            return List.of("Griefing", "Cheating", "Offensive language", "Spam", "Trolling");
+        }
+        
+        return List.of();
+    }
+}
