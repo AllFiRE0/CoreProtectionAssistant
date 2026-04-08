a295a32a63a6d93efae47c5e7a37da58650572cf
AllF1RE <111210671+AllFiRE0@users.noreply.github.com>
2026-04-08 18:32:04 +0300

    Update CoreProtectionAssistant.java

--- src/main/java/ru/allfire/coreprotectionassistant/CoreProtectionAssistant.java
+++ src/main/java/ru/allfire/coreprotectionassistant/CoreProtectionAssistant.java
@@ -14,67 +14,65 @@ import ru.allfire.coreprotectionassistant.managers.*;
 import java.util.logging.Level;
 
 public final class CoreProtectionAssistant extends JavaPlugin {
-
+    
     private static CoreProtectionAssistant instance;
-
+    
     private ConfigManager configManager;
     private DatabaseManager databaseManager;
     private CoreProtectHook coreProtectHook;
     private PapiHook papiHook;
-
+    
     private StaffManager staffManager;
     private WarnManager warnManager;
     private ReportManager reportManager;
     private ChatRuleManager chatRuleManager;
     private AbuseScoreManager abuseScoreManager;
     private ChatBotManager chatBotManager;
-
+    
     private long loadTime;
-
+    
     @Override
     public void onEnable() {
         instance = this;
         loadTime = System.currentTimeMillis();
-
+        
         getLogger().info("§8[§cCoreProtectionAssistant§8] §7Starting...");
-
+        
         if (!loadConfigurations()) {
             getServer().getPluginManager().disablePlugin(this);
             return;
         }
-
+        
         if (!initializeDatabase()) {
             getServer().getPluginManager().disablePlugin(this);
             return;
         }
-
+        
         initializeHooks();
         initializeManagers();
         registerListeners();
         registerCommands();
         registerPlaceholders();
         startScheduledTasks();
-
+        
         new Metrics(this, 22800);
-
+        
         loadTime = System.currentTimeMillis() - loadTime;
         getLogger().info("§8[§cCoreProtectionAssistant§8] §aEnabled in §f" + loadTime + "ms");
         getLogger().info("§8[§cCoreProtectionAssistant§8] §7Author: §fAllF1RE");
     }
-
+    
     @Override
     public void onDisable() {
         if (databaseManager != null) {
             databaseManager.close();
         }
-
         if (papiHook != null) {
             papiHook.unregister();
         }
-
         getLogger().info("§8[§cCoreProtectionAssistant§8] §cDisabled");
     }
-
+    
     private boolean loadConfigurations() {
         try {
             configManager = new ConfigManager(this);
@@ -86,7 +84,7 @@ public final class CoreProtectionAssistant extends JavaPlugin {
             return false;
         }
     }
-
+    
     private boolean initializeDatabase() {
         try {
             databaseManager = new DatabaseManager(this);
@@ -96,14 +94,13 @@ public final class CoreProtectionAssistant extends JavaPlugin {
             return false;
         }
     }
-
+    
     private void initializeHooks() {
         coreProtectHook = new CoreProtectHook(this);
         coreProtectHook.init();
-
         papiHook = new PapiHook(this);
     }
-
+    
     private void initializeManagers() {
         staffManager = new StaffManager(this);
         warnManager = new WarnManager(this);
@@ -111,99 +108,58 @@ public final class CoreProtectionAssistant extends JavaPlugin {
         chatRuleManager = new ChatRuleManager(this);
         abuseScoreManager = new AbuseScoreManager(this);
         chatBotManager = new ChatBotManager(this);
-
         if (chatBotManager.isEnabled()) {
             getLogger().info("§8[§cCoreProtectionAssistant§8] §aChatBot enabled with §f" + 
                 chatBotManager.getRulesCount() + " §arules");
         }
     }
-
+    
     private void registerListeners() {
         getServer().getPluginManager().registerEvents(new ChatListener(this), this);
         getServer().getPluginManager().registerEvents(new CommandListener(this), this);
         getServer().getPluginManager().registerEvents(new PlayerListener(this), this);
         getServer().getPluginManager().registerEvents(new GriefListener(this), this);
     }
-
+    
     private void registerCommands() {
         CommandManager commandManager = new CommandManager(this);
         commandManager.registerAll();
     }
-
+    
     private void registerPlaceholders() {
         if (getServer().getPluginManager().isPluginEnabled("PlaceholderAPI")) {
             papiHook.register();
         }
     }
-
+    
     private void startScheduledTasks() {
         int interval = configManager.getMainConfig().getInt("warn_clear.check_interval_ticks", 12000);
-
         getServer().getScheduler().runTaskTimerAsynchronously(this,
-            () -> warnManager.checkWarnClearConditions(),
-            interval, interval
-        );
-
+            () -> warnManager.checkWarnClearConditions(), interval, interval);
         getServer().getScheduler().runTaskTimerAsynchronously(this,
-            () -> reportManager.cleanupOldReports(),
-            20 * 60 * 30, 20 * 60 * 60 * 6
-        );
+            () -> reportManager.cleanupOldReports(), 20 * 60 * 30, 20 * 60 * 60 * 6);
     }
-
+    
     public void reload() {
         configManager.reloadAll();
         Lang.setLang(configManager.getLangConfig());
         chatRuleManager.loadRules();
         reportManager.reload();
-
         if (chatBotManager != null) {
             chatBotManager.reload();
         }
-
         getLogger().info("§8[§cCoreProtectionAssistant§8] §aConfiguration reloaded");
     }
-
-    public static CoreProtectionAssistant getInstance() {
-        return instance;
-    }
-
-    public ConfigManager getConfigManager() {
-        return configManager;
-    }
-
-    public DatabaseManager getDatabaseManager() {
-        return databaseManager;
-    }
-
-    public CoreProtectHook getCoreProtectHook() {
-        return coreProtectHook;
-    }
-
-    public StaffManager getStaffManager() {
-        return staffManager;
-    }
-
-    public WarnManager getWarnManager() {
-        return warnManager;
-    }
-
-    public ReportManager getReportManager() {
-        return reportManager;
-    }
-
-    public ChatRuleManager getChatRuleManager() {
-        return chatRuleManager;
-    }
-
-    public AbuseScoreManager getAbuseScoreManager() {
-        return abuseScoreManager;
-    }
-
-    public ChatBotManager getChatBotManager() {
-        return chatBotManager;
-    }
-
-    public long getLoadTime() {
-        return loadTime;
-    }
+    
+    public static CoreProtectionAssistant getInstance() { return instance; }
+    public ConfigManager getConfigManager() { return configManager; }
+    public DatabaseManager getDatabaseManager() { return databaseManager; }
+    public CoreProtectHook getCoreProtectHook() { return coreProtectHook; }
+    public StaffManager getStaffManager() { return staffManager; }
+    public WarnManager getWarnManager() { return warnManager; }
+    public ReportManager getReportManager() { return reportManager; }
+    public ChatRuleManager getChatRuleManager() { return chatRuleManager; }
+    public AbuseScoreManager getAbuseScoreManager() { return abuseScoreManager; }
+    public ChatBotManager getChatBotManager() { return chatBotManager; }
+    public long getLoadTime() { return loadTime; }
 }
